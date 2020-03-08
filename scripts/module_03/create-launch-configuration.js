b1caca1a16b56d561645dcaac9e17637667987d3
GlenMiskimmons <42587792+GlenMiskimmons@users.noreply.github.com>
2020-03-08 17:14:51 +0000

    Topic 4: Scalable Computing in AWS
    Creating a Launch Configuration

--- scripts/module_03/create-launch-configuration.js
+++ scripts/module_03/create-launch-configuration.js
@@ -1,10 +1,12 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: add your region */' })
+// AWS.config.update({ region: '/* TODO: add your region */' })
+AWS.config.update({ region: 'us-east-1' })
 
 // Declare local variables
 // TODO: Create an autoscaling object
+const autoScaling = new AWS.AutoScaling()
 
 const lcName = 'hamsterLC'
 const roleName = 'hamsterLCRole'
@@ -17,4 +19,22 @@ helpers.createIamRole(roleName)
 
 function createLaunchConfiguration (lcName, profileArn) {
   // TODO: Create a launch configuration
+  const params = {
+    IamInstanceProfile: profileArn,
+    ImageId: 'ami-097f8549c0535cac7',
+    InstanceType: 't2.micro',
+    LaunchConfigurationName: lcName,
+    KeyName: keyName,
+    SecurityGroups: [
+      sgName
+    ],
+    UserData: 'IyEvYmluL2Jhc2gNCnN1ZG8gYXB0LWdldCB1cGRhdGUNCnN1ZG8gYXB0LWdldCAteSBpbnN0YWxsIGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQgL2hvbWUvYml0bmFtaS9oYmZsDQpjaG93biAtUiBiaXRuYW1pOiAvaG9tZS9iaXRuYW1pL2hiZmwNCmNkIC9ob21lL2JpdG5hbWkvaGJmbA0Kc3VkbyBucG0gaQ0Kc3VkbyBucG0gcnVuIHN0YXJ0'
+  }
+
+  return new Promise((resolve, reject) => {
+    autoScaling.createLaunchConfiguration(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
